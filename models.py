8f3973e6b36821e2df3be8f485d288af93e85092
mohamed <m_kara@estin.dz>
2025-04-02 20:53:32 +0100

    adding Item model

--- models.py
+++ models.py
@@ -12,3 +12,15 @@ class Recipe(db.Model):
 
     def __repr__(self):
         return f"{self.name} - {self.page_link}"
+    
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(10000), unique=True, nullable=False)
+    rating = db.Column(db.Integer)
+    cost = db.Column(db.String(10), nullable=False)
+    time = db.Column(db.Integer)  # in minutes
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+    
